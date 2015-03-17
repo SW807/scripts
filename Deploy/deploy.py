84ea3ce35db259e970b8bbbecabcabaff7070b76
Lasse Vang Gravesen <gravesenlasse@gmail.com>
2015-03-17 13:36:30 +0100

    Deploy script now works.

---
+++ Deploy/deploy.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import subprocess
+
+def main():
+    root_directory = "C:\Users\Mink\Subversion\P8\P8-Modules"
+    app_directories = ["PsyLog",
+                       "module-accelerometer",
+                       "module-gyroscope",
+                       "module-light"
+                       "module-location"
+                       "module-proximity"
+                       "module-screen"
+                       "module-sound",
+                       "module-testanalyse"]
+    # To build:     gradlew.bat build el. gradlew.bat clean build -> outputter apk'er som kan installeres.
+    # To install:   adb -d install apk-path
+    original_dir = os.getcwd()
+    for app in [os.path.join(root_directory, x) for x in app_directories]:
+        print("Building application %s" % app)
+        os.chdir(app)
+        gradlew_path = os.path.join(app, "gradlew.bat")
+        subprocess.Popen([gradlew_path, "build"]).wait()
+        
+        apk_path = os.path.join(app, "app\\build\\outputs\\apk\\app-debug.apk")
+        if(os.path.isfile(apk_path)):
+            print("Installing application %s" % app)
+            subprocess.Popen(["adb", "install", "-r", apk_path]).wait()
+        os.chdir(original_dir)
+        
+        
+    
+                 
+    
+if __name__ == '__main__':
+    main()
