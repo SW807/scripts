c598de2c3191fbe5fac2443ce574d8afb8932b45
Mathias Winde <Mapede11@student.aau.dk>
2015-03-18 09:36:02 +0100

    Ændrede det lige så det virkede, for det kunne Lasse ikke lige :D

--- Deploy/deploy.py
+++ Deploy/deploy.py
@@ -3,14 +3,14 @@ import sys
 import subprocess
 
 def main():
-    root_directory = "C:\Users\Mink\Subversion\P8\P8-Modules"
+    root_directory = "C:\Users\Mathias\Desktop\P8\Programmer"
     app_directories = ["PsyLog",
                        "module-accelerometer",
                        "module-gyroscope",
-                       "module-light"
-                       "module-location"
-                       "module-proximity"
-                       "module-screen"
+                       "module-light",
+                       "module-location",
+                       "module-proximity",
+                       "module-screen",
                        "module-sound",
                        "module-testanalyse"]
     # To build:     gradlew.bat build el. gradlew.bat clean build -> outputter apk'er som kan installeres.
@@ -20,7 +20,7 @@ def main():
         print("Building application %s" % app)
         os.chdir(app)
         gradlew_path = os.path.join(app, "gradlew.bat")
-        subprocess.Popen([gradlew_path, "build"]).wait()
+        subprocess.Popen([gradlew_path, "assembleDebug"]).wait()
         
         apk_path = os.path.join(app, "app\\build\\outputs\\apk\\app-debug.apk")
         if(os.path.isfile(apk_path)):
