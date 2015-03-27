7bb5c1f2095d5156743172daf2b7134f5e957ee9
Bruno Thalmann <mr.thalmann@gmail.com>
2015-03-27 10:45:33 +0100

    renamed file

--- ChangeAndroidStudioSolutionName/create_module_template.py
+++ ChangeAndroidStudioSolutionName/create_module_template.py
@@ -1,6 +1,11 @@
 import sys
+import subprocess
+import os
 
+cmd = "git clone https://github.com/SW807/module-template.git"
+result = subprocess.Popen(cmd).wait()
 jegermegasej = sys.argv[1]
+os.chdir("module-template")
 f1 = open(".idea/.name", "w")
 f1.write(jegermegasej)
 f1.close()
@@ -11,3 +16,4 @@ f3 = open("./app/src/main/AndroidManifest.xml", "w")
 f3.write(someshit)
 f3.close()
 print "Renamed solution to: " + sys.argv[1]
+os.chdir("..")
